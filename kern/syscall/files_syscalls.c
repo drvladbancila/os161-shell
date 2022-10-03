b0864ba118e97da75c51b2fc9c02cb12365c473e
drvladbancila <vlad.bancila@live.com>
2022-10-03 12:32:51 +0200

    [SYSCALL] remove wrong header in files_syscalls.c file

--- kern/syscall/files_syscalls.c
+++ kern/syscall/files_syscalls.c
@@ -29,26 +29,8 @@
 
 
 /*
- * Code to load an ELF-format executable into the current address space.
- *
- * It makes the following address space calls:
- *    - first, as_define_region once for each segment of the program;
- *    - then, as_prepare_load;
- *    - then it loads each chunk of the program;
- *    - finally, as_complete_load.
- *
- * This gives the VM code enough flexibility to deal with even grossly
- * mis-linked executables if that proves desirable. Under normal
- * circumstances, as_prepare_load and as_complete_load probably don't
- * need to do anything.
- *
- * If you wanted to support memory-mapped executables you would need
- * to rearrange this to map each segment.
- *
- * To support dynamically linked executables with shared libraries
- * you'd need to change this to load the "ELF interpreter" (dynamic
- * linker). And you'd have to write a dynamic linker...
- */
+*  Implementation of the system calls related to file management.
+*/
 
 #include <types.h>
 #include <copyinout.h>
