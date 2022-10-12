328c05daa47fd6db4982a574630961b3aff6510c
drvladbancila <vlad.bancila@live.com>
2022-10-12 19:10:52 +0200

    [SYSCALL] execv now seems to work decently and it is commented!

--- kern/syscall/proc_syscalls.c
+++ kern/syscall/proc_syscalls.c
@@ -37,7 +37,7 @@
 #include <mips/trapframe.h>
 #include <copyinout.h>
 
-static const char *arg_padding[4] = {"\0", "\0\0", "\0\0\0", "\0\0\0\0"};
+static const char *arg_padding[] = {"", "\0", "\0\0", "\0\0\0"};
 
 /*
 * System call interface function to get the process ID
@@ -108,33 +108,93 @@ int
 sys_execv(userptr_t prog, userptr_t args)
 {
     char *kprogname;
-    char *kargv[10];
+    size_t prog_len;
+    /*
+    * this is just a cast from a userptr_t to a double pointer to char
+    * so that we can use [] and treat it as if it was the usual argv
+    */
     char **argv = (char **) args;
-
+    /*
+    * kbuf is the kernel buffer where we put everything
+    * kbuf_ptr_begin points to the beginning of the data (arguments)
+    * kbuf_ptr is moved while copying the arguments that are passed
+    */
+    char **kbuf, *kbuf_ptr, *kbuf_ptr_begin;
+    size_t kbuf_size;
     int result;
     int i = 0;
+    size_t args_size = 0, arg_size, padding_size;
     int argc = 0;
     struct vnode *v;
     struct addrspace *as;
-    vaddr_t entrypoint, stackptr;
+    vaddr_t entrypoint, stackptr, stackptr_data, stackptr_argv;
 
+    /* if program name is an invalid buffer */
+    prog_len = strlen((char *) prog);
+    if (check_buffer(prog, prog_len)) {
+        return EFAULT;
+    }
     /* copy program name into kernel space */
     kprogname = kstrdup((char *) prog);
 
-    (void) arg_padding;
-
-    /* copy arguments into kernel space */
+    /* here we copy the arguments to kernel space */
 
     /* count number of arguments */
-    while ((argv+argc) != NULL){
+    while (argv[argc] != NULL && args_size <= ARG_MAX){
+        arg_size = strlen(argv[argc]) + 1;
+        if (check_buffer((userptr_t) argv[argc], arg_size)) {
+            return EFAULT;
+        }
+        if (arg_size % 4 == 0) {
+            /* no need for padding */
+            padding_size = 0;
+        } else {
+            /* need for padding */
+            padding_size = 4 - (arg_size % 4);
+        }
+        args_size += arg_size + padding_size;
         argc++;
     }
 
-    for (i = 0; i < argc; i++) {
-        kargv[i] = kstrdup(*(argv+argc));
+    /* size of arguments (with padding) is too large */
+    if (args_size > ARG_MAX) {
+        return E2BIG;
     }
 
-    (void) kargv;
+    /*
+    * kbuf must be big enough to accomodate the args with padding and an
+    * initial pointer to the data which is made of (argc + 1) words
+    */
+    kbuf_size = sizeof(char) * args_size + sizeof(char *) * (argc + 1);
+    /* allocate the buffer in the kernel heap */
+    kbuf  = (char **) kmalloc(kbuf_size);
+    /* point to the part where you will store the data (padded args) */
+    kbuf_ptr_begin = (char *) (kbuf + argc + 1);
+    /* copy the pointer so that you do not lose reference to the beginning */
+    kbuf_ptr = kbuf_ptr_begin;
+
+    /* for each argument ... */
+    for (i = 0; i < argc; i++) {
+        /* set the vector entry i to the value of the ptr where first arg begins */
+        kbuf[i] = kbuf_ptr;
+        /* compute arg size and check if it needs padding */
+        arg_size = strlen(argv[i]) + 1;
+        if (arg_size % 4 == 0) {
+            padding_size = 0;
+        } else {
+            padding_size = 4 - (arg_size % 4);
+        }
+        /* copy the argument into the data part of the kernel buffer */
+        copyin((userptr_t) argv[i], kbuf_ptr, arg_size);
+        kbuf_ptr += arg_size;
+        /* if there is padding to do, concatenate the padding string */
+        if (padding_size > 0) {
+            memcpy(kbuf_ptr, arg_padding[padding_size], padding_size);
+        }
+        kbuf_ptr += padding_size;
+    }
+    /* last element of the reference vector must be null also in kern buffer */
+    kbuf[argc] = NULL;
 
     /* open ELF file */
     result = vfs_open(kprogname, O_RDONLY, 0, &v);
@@ -160,19 +220,42 @@ sys_execv(userptr_t prog, userptr_t args)
         return result;
     }
 
-    /* close ELF file */
+    /* close ELF file, we have loaded it into memory */
     vfs_close(v);
 
-    /* define user stack in the new address space, simply assigns stackptr to 0x80000000 */
+    /* define user stack in the new address space: simply assigns stackptr to 0x80000000 */
     result = as_define_stack(as, &stackptr);
     if (result) {
         return result;
     }
 
-    // TODO copy arguments to user stack
+    /*
+    * the buffer will start at top of stack - the size of the buffer that we need to
+    * load into the new userspace
+    */
+    stackptr_argv = stackptr - kbuf_size;
+    /* the data segment of the userspace buffer starts (argc + 1) words later */
+    stackptr_data = stackptr_argv + sizeof(char *) * (argc + 1);
+    /*
+    * NOTE! Before copying everything brutally into the new userspace, we need to
+    * change the references so that they point to where things will be copied
+    * into the userspace
+    */
+    for (i = 0; i < argc; i++) {
+        /*
+        * This is just a mapping between kernel buffer and addrspace buffer:
+        * compute the delta between where argv[i] starts and where the data segment
+        * starts in the kernel buffer. Add this delta to where the data segment
+        * starts in the userspace
+        */
+        kbuf[i] = (kbuf[i] - kbuf_ptr_begin) + (char *) stackptr_data;
+    }
+
+    /* now we can copy everything brutally as the references will be correct! */
+    copyout(kbuf, (userptr_t) stackptr_argv, kbuf_size);
 
     /* switch to user mode */
-    enter_new_process(argc, argv, NULL, stackptr, entrypoint);
+    enter_new_process(argc, (userptr_t) stackptr_argv, NULL, stackptr_argv, entrypoint);
     panic("enter_new_process returned in execv\n");
     return EINVAL;
 }
